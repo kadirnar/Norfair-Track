eff70403508673167d432c8b2eb344d0eaa2e046
kadirnar <kadir.nar@hotmail.com>
2022-11-28 01:55:26 +0300

    The update module has been updated.

--- norfair_tracker/__init__.py
+++ norfair_tracker/__init__.py
@@ -1,3 +1,3 @@
-from norfair_tracker.norfair import NorFair
+from norfair_tracker.norfair import NorFairTracker
 
-__version__ = "0.0.2"
+__version__ = "0.0.4"
--- norfair_tracker/norfair.py
+++ norfair_tracker/norfair.py
@@ -1,58 +1,75 @@
+# https://github.com/tryolabs/norfair/blob/master/demos/yolov5/src/demo.py
+
+from typing import Callable, Optional, Union
+from norfair.filter import OptimizedKalmanFilterFactory, FilterFactory
+from norfair import Tracker, Detection
 import numpy as np
-from norfair import Detection, Tracker
 
-
-class NorFair:
-    def __init__(self, detector, max_distance_between_points=30) -> None:
-
+class NorFairTracker:
+    def __init__(
+        self,
+        distance_function: Union[str, Callable] = "frobenius",
+        distance_threshold: float = 0.3,
+        hit_counter_max: int = 15,
+        initialization_delay: Optional[int] = None,
+        pointwise_hit_counter_max: int = 4,
+        detection_threshold: float = 0,
+        filter_factory: FilterFactory = OptimizedKalmanFilterFactory(),
+        past_detections_length: int = 4,
+        reid_distance_function: Optional[Union[str, Callable]] = None,
+        reid_distance_threshold: float = 0,
+        reid_hit_counter_max: Optional[int] = None,
+    ):
         self.tracker = Tracker(
-            distance_function=self._euclidean_distance,
-            distance_threshold=max_distance_between_points,
+            distance_function=distance_function,
+            distance_threshold=distance_threshold,
+            hit_counter_max=hit_counter_max,
+            initialization_delay=initialization_delay,
+            pointwise_hit_counter_max=pointwise_hit_counter_max,
+            detection_threshold=detection_threshold,
+            filter_factory=filter_factory,
+            past_detections_length=past_detections_length,
+            reid_distance_function=reid_distance_function,
+            reid_distance_threshold=reid_distance_threshold,
+            reid_hit_counter_max=reid_hit_counter_max,
         )
-        self.detector = detector
-        try:
-            self.input_shape = tuple(detector.model.get_inputs()[0].shape[2:])
-        except AttributeError as e:
-            self.input_shape = (640, 640)
+        
+    
+    def update(self, dets, _):
+        xyxys = dets[:, 0:4].numpy()
+        confs = dets[:, 4].numpy()
+        category_ids = dets[:, 5].numpy()
 
-    def _euclidean_distance(self, detection, tracked_object):
-        return np.linalg.norm(detection.points - tracked_object.estimate)
+        output_results = np.column_stack((xyxys, confs, category_ids))
 
-    def detect_and_track(self, image: np.ndarray, filter_classes: list = None):
-        _dets_xyxy, image_info = self.detector.detect(
-            image, input_shape=self.input_shape, filter_classes=filter_classes
-        )
-
-        class_ids = []
-        ids = []
-        bboxes_xyxy = []
-        scores = []
-
-        if isinstance(_dets_xyxy, np.ndarray) and len(_dets_xyxy) > 0:
-
-            dets_xyxy = [
-                Detection(np.array([(box[2] + box[0]) / 2, (box[3] + box[1]) / 2]), data=box)
-                for box in _dets_xyxy
-                # if box[-1] == 2
-            ]
-
-            bboxes_xyxy, ids, scores, class_ids = self._tracker_update(dets_xyxy, image_info)
-
-        return bboxes_xyxy, ids, scores, class_ids
-
-    def _tracker_update(self, dets_xyxy: list, image_info: dict):
-
-        bboxes_xyxy = []
-        class_ids = []
-        scores = []
-        ids = []
-
-        tracked_objects = self.tracker.update(detections=dets_xyxy)
-
+        norfair_detections = []
+        for ind in range(len(output_results)):
+            bbox = np.array([[xyxys[ind][0].item(), xyxys[ind][1].item()],[xyxys[ind][2].item(), xyxys[ind][3].item()]])
+            scores = np.array([confs[ind].item(), confs[ind].item()])
+            category_id = int(category_ids[ind])
+            norfair_detections.append(
+                Detection(
+                    bbox,
+                    scores,
+                    label=category_id,
+                )
+            )
+        tracked_objects = self.tracker.update(norfair_detections)
+        outputs = []
         for obj in tracked_objects:
-            det = obj.last_detection.data
-            bboxes_xyxy.append(det[:4])
-            class_ids.append(int(det[-1]))
-            scores.append(int(det[-2]))
-            ids.append(obj.id)
-        return np.array(bboxes_xyxy), ids, scores, class_ids
+            if not obj.live_points.any():
+                continue
+            
+            points = obj.estimate[obj.live_points]
+            points = points.astype(int)
+            x1, y1, x2, y2 = points[0][0], points[0][1], points[1][0], points[1][1]
+            track_id = obj.id
+            cls_id = obj.label
+            score =  obj.last_detection.scores[0] 
+            outputs.append(np.array([x1, y1, x2, y2, track_id, cls_id, score]))
+        if len(outputs) > 0:
+            outputs = np.stack(outputs, axis=0)
+        else:
+            outputs = np.empty((0, 7))
+        return outputs
+        
